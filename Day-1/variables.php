4623b680d6777187763f8625e00858ecf810375a
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2023-08-11 03:10:15 +0600

    php variables

---
+++ Day-1/variables.php
@@ -0,0 +1,16 @@
+<?php
+// Variable
+$firstName = "Adrite";
+$lastName = "Adhikary";
+$age = 7;
+$isStudent = true;
+$marks = array(95, 99, 98);
+$fullName = "{$firstName} {$lastName}";
+
+// Constants
+define("PI", 3.14159);
+
+// Output
+echo $fullName;
+echo "\n";
+echo PI;
